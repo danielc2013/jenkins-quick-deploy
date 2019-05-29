03513e497d9a32c3478ce070109434036ba9858e
David Freeman <david.freeman12@gmail.com>
2019-05-28 20:31:30 -0600

    update include in default.rb recipe

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
-include_recipe 'jenkins::default'
+include_recipe 'jenkins::master'
 
 jenkins_command 'jenkins_start' do
   action :start

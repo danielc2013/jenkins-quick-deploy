e08001bd95ba4ecbac4ca5a97a6ee9c424539f06
David Freeman <david.freeman12@gmail.com>
2019-05-28 21:08:07 -0600

    update default.rb and add SG rule for 8080 traffic

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -3,8 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe 'jenkins::java'
 include_recipe 'jenkins::master'
 
 jenkins_command 'jenkins_start' do
-  action :start
+  action :execute
 end

60d5afd323f093ff4c2d0ff273342872fb028d6f
David Freeman <david.freeman12@gmail.com>
2019-06-01 15:56:33 -0600

    create jenkins user in default cookbook

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -5,3 +5,8 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 include_recipe 'jenkins::java'
 include_recipe 'jenkins::master'
+
+jenkins_user 'dfreeman' do
+  full_name 'David Freeman'
+  password  'password'
+end

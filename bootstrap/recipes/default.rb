85c2e4263d77bb049665a194871de0513c23c67e
danielc2013 <danielcasias@holos.solutions>
2019-06-16 11:51:41 -0600

    rollback broken code

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -5,8 +5,3 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 include_recipe 'jenkins::java'
 include_recipe 'jenkins::master'
-
-jenkins_user 'dfreeman' do
-  full_name 'David Freeman'
-  password  'password'
-end

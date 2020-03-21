806cb6db8ec3d751db4a330d8b0e039f91af47ea
daniel casias <danielcasias@holos.solutions>
2020-03-21 08:34:22 -0600

    stash old changes

--- bootstrap/recipes/default.rb
+++ bootstrap/recipes/default.rb
@@ -6,6 +6,8 @@
 include_recipe 'java::openjdk'
 include_recipe 'jenkins::master'
 
-jenkins_plugin 'pipeline' do
-  action :install
+# jenkins_plugin 'workflow-aggregator'
+
+execute 'install pipeline plugin' do
+  command "java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 -http install-plugin workflow-aggregator"
 end
